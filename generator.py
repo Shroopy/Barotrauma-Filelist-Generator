2aee0eeac07f139a054722b48df3ac1092371fa6
Shroopy <46693163+Shroopy@users.noreply.github.com>
2025-10-15 04:19:14 -0700

    more work

--- generator.py
+++ generator.py
@@ -17,13 +17,61 @@ for rootdir, dirs, files in Path(targetDir).walk():
 			tree = ET.parse(str(rootdir) + "/" + file)
 			root = tree.getroot()
 
-			tagtypes = ("Item", "Character", "Afflictions", "BackgroundCreaturePrefabs", "BallastFlora", "CaveGenerationParameters", "Corpses", "Decals", "EventManagerSettings", "Factions", "ItemAssembly", "Jobs", "LevelGenerationParameters", "LevelObjectPrefabs", "LocationTypes", "MapGenerationParameters", "Missions", "NPCConversations", "NPCPersonalityTraits", "NPCSets", "Orders", "OutpostConfig", "Particles", "RandomEvents", "RuinConfig", "ServerExecutable", "SkillSettings", "Sounds", "StartItems", "Structure", "Talents", "TalentTrees", "Text", "TraitorMissions", "Tutorials", "UIStyle", "UpgradeModules", "WreckAIConfig", "Other", "infotexts", "style")
-			if root.tag.lower() not in [s.lower() for s in tagtypes]:
+			tagtypes = {
+				"Items" : "Item",
+				"Item" : "Item",
+				"Character" : "Character",
+				"Afflictions" : "Afflictions",
+				"backgroundcreatures" : "BackgroundCreaturePrefabs",
+				"BallastFloraBehavior" : "BallastFlora",
+				"CaveGenerationParameters" : "CaveGenerationParameters", 
+				"Corpses" : "Corpses", 
+				#"prefabs" : "Decals", 
+				"EventManagerSettings" : "EventManagerSettings", 
+				"Factions" : "Factions",
+				"ItemAssembly" : "ItemAssembly", 
+				"Jobs" : "Jobs", 
+				"LevelGenerationParameters" : "LevelGenerationParameters", 
+				"levelobjects" : "LevelObjectPrefabs",
+				"locationtypes" : "LocationTypes",
+				"MapGenerationParameters" : "MapGenerationParameters",
+				"Missions" : "Missions", 
+				"Conversations" : "NPCConversations", 
+				"PersonalityTraits" : "NPCPersonalityTraits",
+				"npcsets" : "NPCSets", 
+				"Orders" : "Orders", 
+				"OutpostConfig" : "OutpostConfig", 
+				"prefabs" : "Particles", 
+				"Randomevents" : "RandomEvents", 
+				"RuinGenerationParameters" : "RuinConfig", 
+				#"ServerExecutable", 
+				"SkillSettings" : "SkillSettings", 
+				"sounds" : "Sounds", 
+				"StartItems" : "StartItems", 
+				#"prefabs" : "Structure", 
+				"Talents" : "Talents", 
+				"TalentTrees" : "TalentTrees", 
+				"infotexts" : "Text", 
+				"TraitorMissions" : "TraitorMissions", 
+				"Tutorials" : "Tutorials", 
+				"style" : "UIStyle", 
+				"UpgradeModules" : "UpgradeModules", 
+				"WreckAIConfig" : "WreckAIConfig",
+				#"Other"
+			}
+			if root.tag not in tagtypes:
 				continue
+			newtag = tagtypes[root.tag]
 			
 			line: str
 			filepath = (str(rootdir.as_posix()) + "/" + file).replace(targetDir, "%ModDir%")
-			fileElement = Element(root.tag, {"file":filepath})
+			fileElement = Element(newtag, {"file":filepath})
+
+			filelist.getroot().append(fileElement) # type: ignore
+		if file.endswith(".sub"):
+			line: str
+			filepath = (str(rootdir.as_posix()) + "/" + file).replace(targetDir, "%ModDir%")
+			fileElement = Element("Submarine", {"file":filepath})
 
 			filelist.getroot().append(fileElement) # type: ignore
 
