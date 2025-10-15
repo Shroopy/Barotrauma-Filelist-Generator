5f7419eadeb97fd1da475666e7aab1fe58bd9189
Shroopy <46693163+Shroopy@users.noreply.github.com>
2025-10-15 03:01:06 -0700

    it works 👍

--- generator.py
+++ generator.py
@@ -1,14 +1,30 @@
 from pathlib import Path
+from xml.etree import ElementTree as ET
+from xml.etree.ElementTree import ElementTree
+from xml.etree.ElementTree import Element
 
-# targetDir = input("Target directory to scan: ")
+targetDir = input("Target directory to scan: ")
+targetDir = Path(targetDir).as_posix()
 
-filelist = ""
+name = targetDir.split("/")[-1]
 
-for root, dirs, files in Path(r"C:\Users\Delyth Rauenzahn\Programming\Barotrauma 40k Reworked").walk():
+filelist = ElementTree(Element("contentpackage", {"name":name}))
+
+for rootdir, dirs, files in Path(targetDir).walk():
 	dirs[:] = [d for d in dirs if not d.startswith('.')]
 	for file in files:
-		filelist += file + "\n"
+		if file.endswith(".xml"):
+			tree = ET.parse(str(rootdir) + "/" + file)
+			root = tree.getroot()
 
-with open("filelist.txt", "w") as f:
-	f.write(filelist)
-		
+			if root.tag not in ("Item", "Character", "Afflictions", "BackgroundCreaturePrefabs", "BallastFlora", "CaveGenerationParameters", "Corpses", "Decals", "EventManagerSettings", "Factions", "ItemAssembly", "Jobs", "LevelGenerationParameters", "LevelObjectPrefabs","LocationTypes","MapGenerationParameters", "Missions", "NPCConversations","NPCPersonalityTraits", "NPCSets", "Orders", "OutpostConfig", "Particles", "RandomEvents", "RuinConfig", "ServerExecutable", "SkillSettings", "Sounds", "StartItems", "Structure", "Talents", "TalentTrees", "Text", "TraitorMissions", "Tutorials", "UIStyle", "UpgradeModules", "WreckAIConfig", "Other"):
+				continue
+			
+			line: str
+			filepath = (str(rootdir.as_posix()) + "/" + file).replace(targetDir, "%ModDir%")
+			fileElement = Element(root.tag, {"file":filepath})
+
+			filelist.getroot().append(fileElement) # type: ignore
+
+ET.indent(filelist, '\t')
+filelist.write("filelist.xml", xml_declaration=True)
